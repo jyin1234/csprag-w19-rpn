278a4f7ef4dd43e3c975c4c347940b92f8419fe6
Jesse Yin <jessyin@umich.edu>
2019-03-11 15:38:24 -0400

    Implement rpn calculator, add test for exponentiation

--- rpn.py
+++ rpn.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 
+def isInt(string):
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
 def calculate(arg):
-    pass
+    stack = []
+    for op in arg.split(" "):
+            if isInt(op):
+                stack.append(int(op))
+            else:
+                if op == '+' and len(stack) > 1:
+                    result = stack[-1] + stack[-2]
+                    stack.pop()
+                    stack.pop()
+                    stack.append(result)
+                elif op == '^' and len(stack) > 1:
+                    result = stack[-1] ** stack[-2]
+                    stack.pop()
+                    stack.pop()
+                    stack.append(result)
+
+    return stack[0]
+                
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        print(calculate(input("rpn calc> ")))
 
 if __name__ == '__main__':
     main()
