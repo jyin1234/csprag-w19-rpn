24591295c2cc410496af42e32cd456846196f9d2
Jesse Yin <jessyin@umich.edu>
2019-03-11 21:08:39 -0400

    fix test to accomodate verbose

--- test_rpn.py
+++ test_rpn.py
@@ -4,17 +4,17 @@ import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        result = rpn.calculate("1 1 +")
+        result = rpn.calculate("1 1 +", False)
         self.assertEqual(2, result)
     def test_subtract(self):
-        result = rpn.calculate("5 3 -")
+        result = rpn.calculate("5 3 -", False)
         self.assertEqual(2, result)
     def test_multiply(self):
-        result = rpn.calculate("5 3 *")
+        result = rpn.calculate("5 3 *", False)
         self.assertEqual(15, result)
     def test_divide(self):
-        result = rpn.calculate("6 3 /")
+        result = rpn.calculate("6 3 /", False)
         self.assertEqual(2, result)
     def test_exponentiation(self):
-        result = rpn.calculate("2 2 ^")
+        result = rpn.calculate("2 2 ^", False)
         self.assertEqual(4, result)
